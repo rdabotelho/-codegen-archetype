a5d0f781af446bd03665608dd35ccffd3d895a6e
Raimundo Botelho <raimundo.botelho@gmail.com>
2023-05-21 14:21:28 -0300

    refactor: update template

--- .codegen/templates/entity.java
+++ .codegen/templates/entity.java
@@ -13,7 +13,7 @@ import com.example.demo.domain.enums.*;
 public class DomainName implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@Column(name = "ATTRIBUTE")
