853832f070df01a83f122755c5f23f8c7db4fb8e
Raimundo Botelho <raimundo.botelho@gmail.com>
2023-05-21 00:48:12 -0300

    refactor: update template

--- .codegen/templates/controller.java
+++ .codegen/templates/controller.java
@@ -10,8 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.net.URI;
 import java.util.List;
@@ -19,7 +17,6 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
-@Tag(name="TagName", description = "TagDescription")
 @Transactional
 public class DomainNameController {
 
@@ -29,7 +26,6 @@ public class DomainNameController {
     private DomainNameService domainNameService;
 
     @PostMapping("/domainNames")
-    @Operation(summary = "Create user", description = "Created user object")
     public ResponseEntity<DomainNameDTO> createDomainName(@RequestBody DomainNameDTO domainName) throws Exception {
         log.debug("REST request to save domainName : {}", domainName);
         if (domainName.getId() != null) {
@@ -44,7 +40,6 @@ public class DomainNameController {
     }
 
     @PutMapping("/domainNames")
-    @Operation(summary = "Updates user", description = "Update an existing user")
     public ResponseEntity<DomainNameDTO> updateDomainName(@RequestBody DomainNameDTO domainName) {
         log.debug("REST request to update domainName : {}", domainName);
         if (domainName.getId() == null) {
@@ -56,7 +51,6 @@ public class DomainNameController {
     }
 
     @GetMapping("/domainNames")
-    @Operation(summary = "Returns all user", description = "Returns all user")
     public List<DomainNameDTO> getAllDomainNames() {
         log.debug("REST request to get all domainNames");
         return domainNameService.getAllDomainNames().stream()
@@ -65,7 +59,6 @@ public class DomainNameController {
     }
 
     @GetMapping("/domainNames/{id}")
-    @Operation(summary = "Find user by ID", description = "Returns a single user")
     public ResponseEntity<DomainNameDTO> getDomainName(@PathVariable Long id) {
         log.debug("REST request to get domainName : {}", id);
         return domainNameService.getDomainNameById(id)
@@ -74,7 +67,6 @@ public class DomainNameController {
     }
 
     @DeleteMapping("/domainNames/{id}")
-    @Operation(summary = "Delete user", description = "Delete an existing user")
     public ResponseEntity<Void> deleteDomainName(@PathVariable Long id) {
         log.debug("REST request to delete domainName : {}", id);
         domainNameService.deleteDomainName(id);
