fc9fc23c876b7638244da5800bb76fc5bd3ccd75
Raimundo Botelho <raimundo.botelho@gmail.com>
2023-05-21 00:49:40 -0300

    refactor: update template

--- .codegen/templates/entity.java
+++ .codegen/templates/entity.java
@@ -1,6 +1,6 @@
 package com.example.demo.domain.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 import java.util.ArrayList;
