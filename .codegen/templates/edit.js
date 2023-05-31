8d411d563acfcecf1df026bde3635232c77cc955
Raimundo Botelho <raimundo.botelho@gmail.com>
2023-05-30 22:12:28 -0300

    refactor: updated templates

--- .codegen/templates/edit.js
+++ .codegen/templates/edit.js
@@ -58,7 +58,7 @@ class EmployeeEdit extends Component {
             console.log(error);
         });
         setTimeout(function () {
-            window.location.href = "/";
+            window.location.href = "/employees";
         }, 500);
     }
 
